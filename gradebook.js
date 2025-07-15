09b50363fd750235783bcfbf47d86b2f9c491ac6
anishpatta248552 <atpatta@my.waketech.edu>
2025-07-14 21:34:32 -0400

    Added Gradebook

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+  // Placeholder: return actual grade data from your database here
+}
+
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+  // Placeholder: use data to dynamically fill the table
+}
+
+// TEMP: Call stubs to test that everything is connected
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
